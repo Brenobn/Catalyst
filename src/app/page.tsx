254d4a365f93b7b645f7b7a2b06ea687e0d654ef
Brenobn <brenobragabn@gmail.com>
2025-09-29 16:57:15 -0300

    Importing IconButton, InputField, InputIcon, InputRoot and starting to use them

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,29 @@
-import { ArrowRight } from "lucide-react";
-import { Button } from "@/components/button";
+import { ArrowRight, Copy, Mail } from 'lucide-react'
+import { Button } from '@/components/button'
+import { IconButton } from '@/components/icon-button'
+import { InputField, InputIcon, InputRoot } from '@/components/input'
 
 export default function Home() {
   return (
     <main>
-      <Button>
+      <Button type="submit">
         Eviar
         <ArrowRight />
       </Button>
+
+      <IconButton>
+        <Copy />
+      </IconButton>
+
+      <div>
+        <InputRoot>
+          <InputIcon>
+            <Mail className="size-5" />
+          </InputIcon>
+
+          <InputField />
+        </InputRoot>
+      </div>
     </main>
-  );
+  )
 }
